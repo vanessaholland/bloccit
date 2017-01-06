415e141f2a527963b5fc6adbc45ba066f3a9078f
vanessa holland <vanessaholland@legalshieldcorp.com>
2017-01-06 12:34:08 -0600

    assignment-17 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,8 @@ require 'random_data'
    )
  end
 
+ Post.find_or_create_by(title:"Hi there", body:"this is the body")
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
