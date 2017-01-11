a8955789eda60523a3ac6d48bd9d3976970257f5
vanessa holland <vanessaholland@legalshieldcorp.com>
2017-01-11 12:47:09 -0600

    checkpoint-22 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
+  resources :topics do
+    resources :posts, except: [:index]
+  end
   resources :advertisements
-  resources :posts
   resources :questions
 
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109183151) do
+ActiveRecord::Schema.define(version: 20170111173559) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20170109183151) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -49,6 +51,14 @@ ActiveRecord::Schema.define(version: 20170109183151) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
