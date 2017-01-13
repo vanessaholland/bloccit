49688cc5236f05c1d163b23f9ca4d46d7816e8b1
vanessa holland <vanessaholland@legalshieldcorp.com>
2017-01-13 09:40:23 -0600

    checkpoint-14 completed

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
   resources :advertisements
   resources :questions
+  resources :users, only: [:new, :create]
 
   get 'about' => 'welcome#about'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112174342) do
+ActiveRecord::Schema.define(version: 20170113152026) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -69,6 +69,14 @@ ActiveRecord::Schema.define(version: 20170112174342) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
