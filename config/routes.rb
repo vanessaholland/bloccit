8a81d0bb1e29ed630aede58366349450bc4122b3
vanessa holland <vanessaholland@legalshieldcorp.com>
2017-01-13 22:29:19 -0600

    checkpoint-26 completed

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :advertisements
   resources :questions
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
 
