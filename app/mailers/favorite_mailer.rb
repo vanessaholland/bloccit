be78824314a7996b4e990915c81a13ef8f7b1f78
vanessa holland <vanessaholland@legalshieldcorp.com>
2017-01-19 09:32:12 -0600

    assignment-31 completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -3,14 +3,23 @@ class FavoriteMailer < ApplicationMailer
 
   def new_comment(user, post, comment)
 
-     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-     headers["References"] = "<post/#{post.id}@your-app-name.example>"
+     headers["Message-ID"] = "<comments/#{comment.id}@bloccit.example>"
+     headers["In-Reply-To"] = "<post/#{post.id}@bloccit.example>"
+     headers["References"] = "<post/#{post.id}@bloccit.example>"
 
      @user = user
      @post = post
      @comment = comment
 
      mail(to: user.email, subject: "New comment on #{post.title}")
+   end
+
+   def new_post(post)
+     headers["Message-ID"] = "<posts/#{post.id}@bloccit.example>"
+     headers["In-Reply-To"] = "<post/#{post.id}@bloccit.example>"
+     headers["References"] = "<post/#{post.id}@bloccit.example>"
+
+     @post = post
+     mail(to: post.user.email, subject: "You're following, #{post.title}!")
    end
 end
